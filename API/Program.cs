4bff2d2d78b98d5b2a793de7508e400f04540cd8
GeorgiKadiev <georgi.k.kadiev@gmail.com>
2024-01-31 10:45:36 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var  AngularApp = "AngularApp";
 
 // Add services to the container.
 
@@ -9,6 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AngularApp,
+                      policy  =>
+                      {
+                          policy.WithOrigins("https://localhost:4200");
+                          policy.AllowAnyHeader();
+                          policy.AllowAnyMethod();
+                      });
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +31,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(AngularApp);
 
 app.MapControllers();
 
